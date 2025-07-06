9cb8353a457152aa55f346e6e459a1344dc8e748
Wendell Bitencourt <bat.delgeorge@hotmail.com>
2025-07-06 10:56:07 -0300

    build: build all packages

--- packages/util/mask/src/index.ts
+++ packages/util/mask/src/index.ts
@@ -1,4 +1,23 @@
-import { isValidDateTime } from "@wbitencourt/util-date";
+function isValidDateTime(input: string) {
+  // Espera-se data no formato "dd/mm/yyyy hh:mm"
+  const regex = /^(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})$/;
+  const match = input.match(regex);
+
+  if (!match) return false;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, day, month, year, hour, minute] = match.map(Number);
+
+  const date = new Date(year, month - 1, day, hour, minute);
+
+  return (
+      date.getFullYear() === year &&
+      date.getMonth() === month - 1 &&
+      date.getDate() === day &&
+      date.getHours() === hour &&
+      date.getMinutes() === minute
+  );
+}
 
 function maskCurrencyBRL(value: string | number) {  
   if (!value) return 'R$ 0,00';
