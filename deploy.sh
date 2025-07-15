d4a7621d61b015c932632feccd0e3743f77fcaa9
Wendell Bitencourt <bat.delgeorge@hotmail.com>
2025-07-14 23:54:59 -0300

    build: building workspace

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,14 @@ git add .
 
 git commit -m "build: building workspace"
 
+cd packages/cli
+
 npm version patch
 
 git commit -m "deploy: update version to $npm_package_version"
 
 git push
 
-cd packages/cli
-
 npm publish --access public
+
+cd ../../
